4f87a460dbc3bfad2b68f0b671d48e8a3e06f720
arennett <a.rennett@gmx.de>
2019-01-01 14:01:54 +0100

    .

--- XToolsLib/src/OledMessage.h
+++ XToolsLib/src/OledMessage.h
@@ -15,17 +15,44 @@
 #define WHITE 1
 #define INVERSE 2
 
-typedef enum {CMD_UPDATE=1,CMD_CLEAR,CMD_RECTANGL,CMD_CIRCLE} tOledCmd;
+typedef enum {
+	CMD_UPDATE = 1, CMD_CLEAR, CMD_LINE, CMD_RECTANGL, CMD_CIRCLE, CMD_BMP_START,CMD_BMP_GAME_SELECT,CMD_BMP_GAME_1,CMD_BMP_GAME_2,CMD_GAME_OVER
+} tOledCmd;
+
+typedef union p {
+	struct {
+		byte color;
+		union {
+			byte v;
+			byte x1;
+		} p0;
+		union {
+			byte v;
+			byte y1;
+		} p1;
+		union {
+			byte v;
+			byte x2;
+			byte r;
+		} p2;
+		union {
+			byte v;
+			byte y2;
+		} p3;
+	} a;
+	byte *buff[5];
+
+} tParams;
 
 class OledMessage {
 public:
 	OledMessage();
-	OledMessage(tOledCmd cmd, const byte* pParams =NULL,size_t paramsSize=0);
+	OledMessage(tOledCmd cmd, const byte* pParams = NULL,
+			size_t paramsSize = 0);
 	virtual ~OledMessage();
-	byte  buffer[8];
+	byte buffer[8];
 
-
-	void  *pNext=NULL;
+	void *pNext = NULL;
 	byte getCmd() {
 		return buffer[0];
 	}
@@ -36,6 +63,10 @@ public:
 
 	byte* getParams() {
 		return &buffer[2];
+	}
+
+	tParams* getTParams() {
+		return (tParams*) &buffer[2];
 	}
 
 };
